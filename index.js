49dd6633a37ec1c51d0fee366f8327e03257b18d
muhammadhim-boom <nmmm04476@gmail.com>
2025-01-17 23:52:52 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-// const addButton = document.getElementById('addButton');
-// const todoList = document.getElementById('todoList');
-// addButton.addEventListener('click', () => {
-//     const newTodo = document.createElement('li');
-//     newTodo.textContent = 'New Task 📝'; 
-//     newTodo.className = 'todo-item';
-//     newTodo.addEventListener('click', () => {
-//         newTodo.remove();
-//     });
-//     todoList.appendChild(newTodo);
-// });
-// const listItems = document.querySelectorAll('ul li');
-// listItems.forEach(item => {
-//     item.addEventListener('click', () => {
-//         item.remove(); 
-//     });
-// });
+const addButton = document.getElementById('addButton');
+const todoList = document.getElementById('todoList');
+addButton.addEventListener('click', () => {
+    const newTodo = document.createElement('li');
+    newTodo.textContent = 'New Task 📝'; 
+    newTodo.className = 'todo-item';
+    newTodo.addEventListener('click', () => {
+        newTodo.remove();
+    });
+    todoList.appendChild(newTodo);
+});
+const listItems = document.querySelectorAll('ul li');
+listItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.remove(); 
+    });
+});
