b5cf16904117bfa2d2ecd64d396ad29359d4da5a
muhammadhim-boom <nmmm04476@gmail.com>
2025-01-19 19:19:28 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -16,3 +16,45 @@ listItems.forEach(item => {
         item.remove(); 
     });
 });
+// script.js
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.textContent = '0';
+}
+
+function appendValue(value) {
+    if (display.textContent === '0' && value !== '.') {
+        display.textContent = value;
+    } else {
+        display.textContent += value;
+    }
+}
+
+function deleteLast() {
+    if (display.textContent.length > 1) {
+        display.textContent = display.textContent.slice(0, -1);
+    } else {
+        display.textContent = '0';
+    }
+}
+
+function calculate() {
+    try {
+        let result = eval(display.textContent);
+        display.textContent = result;
+    } catch {
+        display.textContent = 'Error';
+    }
+}
+const button = document.getElementById('add-image-btn');
+const imageContainer = document.querySelector('.image-container');
+
+// "CLICK ME" tugmasi bosilganda rasmni ko'paytirish
+button.addEventListener('click', () => {
+  const newImage = document.createElement('img');
+  newImage.src = 'your-image.jpg'; // Tasvir manzilini shu yerda o'zgartiring
+  newImage.alt = 'Dynamic';
+  newImage.style.borderRadius = '10px';
+  imageContainer.appendChild(newImage);
+});
